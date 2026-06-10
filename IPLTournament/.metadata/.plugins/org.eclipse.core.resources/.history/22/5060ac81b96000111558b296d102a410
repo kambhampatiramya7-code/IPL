d1e6c54b135f997b6e637b371feac41bec6eceda
jhansibhavanasi121 <you@example.com>
2026-06-10 20:46:28 +0530

    Add IPL Tournament Management System project

---
+++ IPLTournament/.metadata/.plugins/org.eclipse.core.resources/.history/22/5060ac81b96000111558b296d102a410
@@ -0,0 +1,10 @@
+package model;
+
+public class Match {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ IPLTournament/IPLTournamentManagementSystem/src/main/java/dao/DBConnection.java
@@ -0,0 +1,21 @@
+package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class DBConnection {
+    static Connection con;
+    public static Connection getConnection() {
+        try {
+            if (con == null) {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/ipl_db",
+                    "root",
+                    "tiger"
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
